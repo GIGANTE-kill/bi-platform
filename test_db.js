5b48e5850906909e5b72e18a2be0284172c95ef5
GIGANTE-kill <matheusextremecardim@gmail.com>
2026-03-09 08:46:35 -0300

    fix. correcao erro

--- test_db.js
+++
@@ -1,8 +0,0 @@
-import { fetchBuilderData } from './src/lib/actions/db.js';
-
-async function test() {
-    const result = await fetchBuilderData("", "");
-    console.log(result.success ? "SUCCESS" : "ERROR", result.error || "");
-}
-
-test();
